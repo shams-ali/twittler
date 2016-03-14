fb067294c6fcca850cddcaad25121062b5101432
Shams Ali <ali.s90@outlook.com>
2016-03-14 07:41:55 -0600

    Completed Minimum Requirements

---
+++ twittler.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  var $timeline = $('#timeline');
+
+  var showTweets = function(user) {
+    var counter = 0;
+    var args = Array.prototype.slice.call(arguments);
+    $(timeline).empty();
+    var endIndex = streams.home.length - 1;
+      for (var i = 0; i <= endIndex; i++) {
+        if(!args.length){
+          user = streams.home[i].user;
+        }
+        if(user === streams.home[i].user){
+          console.log("hi");
+          var tweet = streams.home[i];
+          var $user = $('<div class="user-container"><a href="#" class="user" data-name="' + tweet.user + '">' + "@" + tweet.user + '</a></div>');
+          var $tweet = $('<div class="tweet">' + tweet.message + '</div>');
+          if(counter % 2 === 0){
+            $tweet.addClass('every-Other-Tweet');
+          }
+          var $today = $('<div class="today">' + tweet.created_at + '</div>');
+          $user.prependTo($tweet);
+          $tweet.prependTo($timeline);
+          $today.appendTo($tweet);
+          counter++;
+        }
+      }
+  };
+  //setTimeout(showTweets, 1000, endIndex + 1); (new tweets without button)
+
+  showTweets();
+  $('button').on('click', function(){
+    showTweets();
+    $('.user').on('click', function(){
+     var $user = $(this).data('name');
+     console.log($user);
+      showTweets($user);
+    });
+  });
+
+  $('.user').on('click', function(){
+     var $user = $(this).data('name');
+     console.log($user);
+     showTweets($user);
+  });
+
+
+});
