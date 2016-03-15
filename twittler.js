c69c1f619f2f7a9c951f7139f8402a164b70a058
Shams Ali <ali.s90@outlook.com>
2016-03-15 01:09:20 -0600

    changed date to elpased time since tweet

--- twittler.js
+++ twittler.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
   var $timeline = $('#timeline');
 
+var elapsedTime = function(endTime, startTime){
+  var elapsedDays = endTime.getDay() - startTime.getDay();
+  var elapsedHours = endTime.getHours() - startTime.getHours();
+  var elapsedMin = endTime.getMinutes() - startTime.getMinutes();
+  if(elapsedDays > 0){
+    return "-Tweeted " + elapsedDays + " day(s) ago";
+  }else if(elapsedHours > 0){
+    return "-Tweeted " + elapsedHours + " hour(s) ago";
+  }else if(elapsedMin > 0){
+    return "-Tweeted " + elapsedMin + " minute(s) ago";
+  }else{
+    return "-Tweeted less than a minute ago";
+  }
+};
+
   var showTweets = function(user) {
     var counter = 0;
     var args = Array.prototype.slice.call(arguments);
-    if(args.length){
-      $(timeline).empty();
-    }
+    $(timeline).empty();
     var endIndex = streams.home.length - 1;
       for (var i = 0; i <= endIndex; i++) {
         if(!args.length){
           user = streams.home[i].user;
         }
         if(user === streams.home[i].user){
-          console.log("hi");
           var tweet = streams.home[i];
+          var endTime = new Date();
+          var startTime = tweet.created_at;
           var $user = $('<div class="user-container"><a href="#" class="user" data-name="' + tweet.user + '">' + "@" + tweet.user + '</a></div>');
           var $tweet = $('<div class="tweet">' + tweet.message + '</div>');
-          var $today = $('<div class="today">' + tweet.created_at + '</div>');
+          var $today = $('<div class="today">' + elapsedTime(endTime, startTime) + '</div>');
           $user.prependTo($tweet);
           $tweet.prependTo($timeline);
           $today.appendTo($tweet);
@@ -38,7 +52,6 @@ $(document).ready(function(){
 
   $(document).on('click', '.user', function(){
      var $user = $(this).data('name');
-     console.log($user);
      showTweets($user);
   });
 
